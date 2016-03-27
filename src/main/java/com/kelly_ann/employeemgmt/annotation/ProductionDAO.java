0ee4fc0851375d6c94084644e2304d8d6e2173a0
specialk1st <kapeart@gmail.com>
2016-03-27 09:20:52 -0400

    Adding ProductionDAO class.

---
+++ src/main/java/com/kelly_ann/employeemgmt/annotation/ProductionDAO.java
@@ -0,0 +1,16 @@
+
+package com.kelly_ann.employeemgmt.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.TYPE, ElementType.METHOD, ElementType.PARAMETER})
+@Qualifier
+public @interface ProductionDAO {
+	// no-op
+}
